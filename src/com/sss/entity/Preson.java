220b08cacc00b8179c1f03388fe9997f927ded02
ssj <947303536@qq.com>
2020-08-16 17:54:16 +0800

    chushihua

---
+++ src/com/sss/entity/Preson.java
@@ -0,0 +1,13 @@
+package com.sss.entity;
+
+/**
+ * @ClassName Preson
+ * @Description TODO
+ * @Author 沈世举
+ * @Date 2020/8/16 17:39
+ * @Version 1.0
+ */
+public class Preson {
+    private String id;
+    private String name;
+}
